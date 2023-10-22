21ac4a72892982eafa967c41ae2714e91fc1edb8
harmonia2side <breno_odnan@hotmail.com>
2023-10-22 20:53:25 -0300

    continua a 2

---
+++ lista1/ml_utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+# Função que calcula probabilidade de um ou mais atributos dentro de um dataset
+#   data: dataset
+#   atribute: a lista de (um ou dois) atributos cuja probabilidade será calculada
+# retorna: um float correspondente à probabilidade requerida
+def probabilidade(data, atribute):
+    print('update2')
+    data['buying'].value_counts()
+    
