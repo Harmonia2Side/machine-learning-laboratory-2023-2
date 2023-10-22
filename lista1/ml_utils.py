4a2b96a961d852145c20cea8502c5a5ae2a48778
harmonia2side <breno_odnan@hotmail.com>
2023-10-22 20:10:43 -0300

    renomeia library customizada

---
+++ lista1/ml_utils.py
@@ -0,0 +1,14 @@
+# Bibliotecas
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+# Função que calcula probabilidade de um ou mais atributos dentro de um dataset
+#   data: dataset
+#   atribute: a lista de (um ou dois) atributos cuja probabilidade será calculada
+# retorna: um float correspondente à probabilidade requerida
+def probabilidade(data, atribute):
+    for i in atribute
+        data[atribute[i]].value_counts()
+        
