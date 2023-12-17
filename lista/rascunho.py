de71565e0ce939fcebe836d6abe4145a95e34308
harmonia2side <breno_odnan@hotmail.com>
2023-12-17 19:24:35 -0300

    finaliza A

---
+++ lista/rascunho.py
@@ -0,0 +1,10 @@
+rascunho
+
+# # Descomente esse bloco caso deseje segmentar os dados
+
+# data_mean = data.loc[:,2:11]
+# print(data_mean)
+# data_std = data.loc[:,12:21]
+# print(data_std)
+# data_largest = data.loc[:,22:31]
+# print(data_largest)
